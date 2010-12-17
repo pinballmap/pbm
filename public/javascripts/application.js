b842b998f289fc3dd1ade4f04efd826c11ea4ce9
Scott Wainstock <ssw@rentrak.com>
2010-12-17 09:34:33 -0800

    drop down for location, moved some js around

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,3 +4,24 @@ $(function () {
     return false;
   });
 });
+
+var map;
+
+function initializeMap() {
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  map = new google.maps.Map(document.getElementById("map_canvas"), { zoom: 8, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP });
+}
+
+function showLocation(lat, lon) {
+console.log(lat);
+console.log(lon);
+  var latlng = new google.maps.LatLng(lat, lon);
+
+  var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title: "Brotastic"
+  });
+
+  map.setCenter(latlng, 0);
+}
