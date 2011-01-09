4134bfcb594ba26dca3f1d0115e68c4cdeefdd38
Scott Wainstock <ssw@rentrak.com>
2011-01-09 15:41:50 -0800

    added full_name to region, reworked the home and region pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110109202100) do
+ActiveRecord::Schema.define(:version => 20110109233904) do
 
   create_table "location_machine_xrefs", :force => true do |t|
     t.datetime "created_at"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20110109202100) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "full_name"
   end
 
   create_table "users", :force => true do |t|
