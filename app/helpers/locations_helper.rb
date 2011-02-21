3d9cf080d8362794bb32dfc542a0807c09315daf
Scott Wainstock <ssw@rentrak.com>
2011-02-20 22:08:46 -0800

    styling for search banners

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,7 +1,7 @@
 module LocationsHelper
   def open_closed_arrows_for(name, id=nil)
-    arrows = "<div id='#{name}_open_arrow#{"_#{id}" if (id)}' class='float_left' style='display: none;'><img alt='open arrow' src='images/open_arrow.gif' /></div>"
-    arrows += "<div id='#{name}_closed_arrow#{"_#{id}" if (id)}' class='float_left'><img alt='closed arrow' src='images/closed_arrow.gif' /></div>"
+    arrows = "<div class='#{'arrow' unless (id)}' id='#{name}_open_arrow#{"_#{id}" if (id)}' style='display: none;'><img alt='open arrow' src='images/open_arrow.gif' /></div>"
+    arrows += "<div class='#{'arrow' unless (id)}' id='#{name}_closed_arrow#{"_#{id}" if (id)}'><img alt='closed arrow' src='images/closed_arrow.gif' /></div>"
 
     arrows.html_safe
   end
@@ -18,7 +18,7 @@ HERE
 
   def search_banner(type, header_text)
     html = <<HERE
-<div id="#{type}_banner" class="sub_nav_item" onclick="hide_search_sections(); toggle_data('#{type}');">
+<div id="#{type}_banner" class="search_banner" onclick="hide_search_sections(); toggle_data('#{type}');">
   <span>#{header_text}</span>
   #{open_closed_arrows_for(type)}
 </div>
--- spec/helpers/locations_helper.rb
+++ spec/helpers/locations_helper.rb
@@ -4,9 +4,9 @@ describe LocationsHelper do
   describe '#open_closed_arrows' do
     it 'should give me some open and closed arrows' do
       l = Factory.create(:location)
-      helper.open_closed_arrows_for('foo', l.id).should == "<div id='foo_open_arrow_#{l.id}' class='float_left' style='display: none;'><img alt='open arrow' src='images/open_arrow.gif' /></div><div id='foo_closed_arrow_#{l.id}' class='float_left'><img alt='closed arrow' src='images/closed_arrow.gif' /></div>"
+      helper.open_closed_arrows_for('foo', l.id).should == "<div class='' id='foo_open_arrow_#{l.id}' style='display: none;'><img alt='open arrow' src='images/open_arrow.gif' /></div><div class='' id='foo_closed_arrow_#{l.id}'><img alt='closed arrow' src='images/closed_arrow.gif' /></div>"
 
-      helper.open_closed_arrows_for('foo').should == "<div id='foo_open_arrow' class='float_left' style='display: none;'><img alt='open arrow' src='images/open_arrow.gif' /></div><div id='foo_closed_arrow' class='float_left'><img alt='closed arrow' src='images/closed_arrow.gif' /></div>"
+      helper.open_closed_arrows_for('foo').should == "<div class='arrow' id='foo_open_arrow' style='display: none;'><img alt='open arrow' src='images/open_arrow.gif' /></div><div class='arrow' id='foo_closed_arrow'><img alt='closed arrow' src='images/closed_arrow.gif' /></div>"
     end
   end
 
@@ -33,7 +33,7 @@ HERE
     it 'should give me a banner' do
       l = Factory.create(:location)
       helper.search_banner('by_cool_type', 'This is a cool type, bro').should == <<HERE
-<div id="by_cool_type_banner" class="sub_nav_item" onclick="hide_search_sections(); toggle_data('by_cool_type');">
+<div id="by_cool_type_banner" class="search_banner" onclick="hide_search_sections(); toggle_data('by_cool_type');">
   <span>This is a cool type, bro</span>
   #{open_closed_arrows_for('by_cool_type')}
 </div>
