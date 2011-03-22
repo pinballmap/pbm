95aec28778480ff2eea21a6735d5dda3b4469abd
Scott Wainstock <ssw@rentrak.com>
2011-03-21 17:24:56 -0700

    javascript style refactor

--- spec/helpers/locations_helper.rb
+++ spec/helpers/locations_helper.rb
@@ -14,14 +14,14 @@ describe LocationsHelper do
     it 'should give me a banner' do
       l = Factory.create(:location)
       helper.banner(l, 'cool_type', 'This is a cool type, bro').should == <<HERE
-<div id="cool_type_banner_#{l.id}" class="sub_nav_item" onclick="toggle_data('cool_type', #{l.id});">
+<div id="cool_type_banner_#{l.id}" class="sub_nav_item" onclick="toggleData('cool_type', #{l.id});">
   <span>This is a cool type, bro</span>
   #{open_closed_arrows_for('cool_type', l.id)}
 </div>
 HERE
 
       helper.banner(nil, 'cool_type', 'This is a cool type, bro').should == <<HERE
-<div id="cool_type_banner" class="sub_nav_item" onclick="toggle_data('cool_type');">
+<div id="cool_type_banner" class="sub_nav_item" onclick="toggleData('cool_type');">
   <span>This is a cool type, bro</span>
   #{open_closed_arrows_for('cool_type')}
 </div>
@@ -33,7 +33,7 @@ HERE
     it 'should give me a banner' do
       l = Factory.create(:location)
       helper.search_banner('by_cool_type', 'This is a cool type, bro').should == <<HERE
-<div id="by_cool_type_banner" class="search_banner" onclick="hide_search_sections(); toggle_data('by_cool_type');">
+<div id="by_cool_type_banner" class="search_banner" onclick="hideSearchSections(); toggleData('by_cool_type');">
   <span>This is a cool type, bro</span>
   #{open_closed_arrows_for('by_cool_type')}
 </div>
