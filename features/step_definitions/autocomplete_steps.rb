87a470f69729ba6137c428f7c037fa52f2f0a0e7
Scott Wainstock <ssw@rentrak.com>
2011-03-31 16:43:07 -0700

    limit location autocomplete to region

--- features/step_definitions/autocomplete_steps.rb
+++ features/step_definitions/autocomplete_steps.rb
@@ -4,6 +4,12 @@ Then /^I should see the following autocomplete options:$/ do |table|
   end
 end
 
+Then /^I should not see the following autocomplete options:$/ do |table|
+  table.raw.each do |row|
+    page.should_not have_xpath("//a[text()='#{row[0]}']")
+  end
+end
+
 When /^I click on the "([^"]*)" autocomplete option$/ do |link_text|
   page.evaluate_script %Q{ $('.ui-menu-item a:contains("#{link_text}")').trigger("mouseenter").click(); }
 end
