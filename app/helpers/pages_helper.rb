6fb9af30ab737d1d1fd4da0ab2ef394dc27353b1
Scott Wainstock <ssw@rentrak.com>
2011-04-05 20:09:52 -0700

    show full region name on dropdown, fix misnamed helper specs

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def other_regions_html(region)
-    html = Region.where('id != ?', region.id).order('name').collect {|other_region| "<li><a href='/#{other_region.name.downcase}'>#{other_region.name}</a></li><div class='clear'></div>"}
+    html = Region.where('id != ?', region.id).order('name').collect {|other_region| "<li><a href='/#{other_region.name.downcase}'>#{other_region.full_name}</a></li><div class='clear'></div>"}
     html.join.html_safe
   end
 end
