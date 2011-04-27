9dae0a413936135a438f3280e14c5d0431af883b
Scott Wainstock <ssw@rentrak.com>
2011-04-26 20:58:12 -0700

    new relic, plus some small speed refactors

--- app/controllers/location_machine_xrefs_controller.rb
+++ app/controllers/location_machine_xrefs_controller.rb
@@ -13,16 +13,12 @@ class LocationMachineXrefsController < InheritedResources::Base
       return
     end
 
-    LocationMachineXref.where(:location => Location.find(params[:location_id]), :machine => machine).first ||
-      LocationMachineXref.create(:location => Location.find(params[:location_id]), :machine => machine)
+    LocationMachineXref.where(:location_id => params[:location_id], :machine => machine).first ||
+      LocationMachineXref.create(:location_id => params[:location_id], :machine => machine)
   end
 
-  def remove_confirmation
-  end
   def create_confirmation
     @lmx = LocationMachineXref.find(params[:id])
-  end
-  def condition_update_confirmation
   end
 
   def destroy
@@ -39,8 +35,14 @@ class LocationMachineXrefsController < InheritedResources::Base
   end
 
   def index
-    @lmxs = apply_scopes(LocationMachineXref).includes(:location, :machine)
+    @lmxs = apply_scopes(LocationMachineXref).includes(:location, :machine, :machine_score_xrefs)
     @lmxs.sort! {|a,b| b.id <=> a.id}
     respond_with(@lmxs)
+  end
+
+  def condition_update_confirmation
+  end
+
+  def remove_confirmation
   end
 end
