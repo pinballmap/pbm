93bd1ff64259c3c4b161fd340bdb28394666fa2f
Scott Wainstock <scott.wainstock@gmail.com>
2012-04-07 16:23:45 -0700

    FactoryGirl updates for FactoryGirl version upgrade

--- spec/helpers/locations_helper_spec.rb
+++ spec/helpers/locations_helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe LocationsHelper do
   describe '#open_closed_arrows' do
     it 'should give me some open and closed arrows' do
-      l = Factory.create(:location)
+      l = FactoryGirl.create(:location)
       helper.open_closed_arrows_for('foo', l.id).should == "<div class='arrow' id='foo_open_arrow_#{l.id}' style='display: none;'><img alt='open arrow' src='/images/open_arrow.gif' /></div><div class='arrow' id='foo_closed_arrow_#{l.id}'><img alt='closed arrow' src='/images/closed_arrow.gif' /></div>"
 
       helper.open_closed_arrows_for('foo').should == "<div class='arrow' id='foo_open_arrow' style='display: none;'><img alt='open arrow' src='/images/open_arrow.gif' /></div><div class='arrow' id='foo_closed_arrow'><img alt='closed arrow' src='/images/closed_arrow.gif' /></div>"
@@ -12,7 +12,7 @@ describe LocationsHelper do
 
   describe '#banner' do
     it 'should give me a banner' do
-      l = Factory.create(:location)
+      l = FactoryGirl.create(:location)
       helper.banner('cool_type', 'This is a cool type, bro', l).should == <<HERE
 <div id="cool_type_banner_#{l.id}" class="sub_nav_item" onclick="toggleData('cool_type', #{l.id});">
   <span>This is a cool type, bro</span>
@@ -31,7 +31,7 @@ HERE
 
   describe '#search_banner' do
     it 'should give me a banner' do
-      l = Factory.create(:location)
+      l = FactoryGirl.create(:location)
       helper.search_banner('by_cool_type', 'This is a cool type, bro').should == <<HERE
 <div id="by_cool_type_banner" class="search_banner">
   <span>This is a cool type, bro</span>
