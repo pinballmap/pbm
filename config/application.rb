23db175b74b0eb413186f6c54be3035cb43584c2
Scott Wainstock <scott.wainstock@gmail.com>
2012-04-08 13:26:01 -0700

    enabled config.assets

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Pbm
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.assets.enabled = true
   end
 end
