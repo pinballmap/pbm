af2d05e3b76e4d9bca5e528076ac550f4c221589
Scott Wainstock <scott.wainstock@gmail.com>
2012-05-26 12:54:47 -0700

    add back history access to users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,7 @@ class Ability
     if user && user.region_id?
       can :access, :rails_admin
       can :dashboard
+      can :history
       if user.region.name == 'portland'
         can :manage, [LocationType, Region, Machine]
         can [:update, :read], [LocationPictureXref], :location => { :region_id => user.region_id }
