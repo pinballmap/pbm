38d5f876eacc143b141ee295ce6cd5791bea6f26
Scott Wainstock <scott.wainstock@gmail.com>
2012-07-13 16:19:27 -0700

    turn off assets.compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,9 +12,6 @@ Pbm::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
-  # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
 
