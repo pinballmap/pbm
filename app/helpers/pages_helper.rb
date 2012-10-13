51db32345aefca139414df2feb9700c9edfd3699
Scott Wainstock <scott.wainstock@gmail.com>
2012-10-13 13:42:00 -0700

    make html validate

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,9 @@
 module PagesHelper
   def other_regions_html(region)
-    html = Region.where('id != ?', region.id).order('name').collect {|other_region| "<li><a href='/#{other_region.name.downcase}'>#{other_region.full_name}</a></li><div class='clear'></div>"}
-    html.join.html_safe
+    html = "<ul>"
+    html << Region.where('id != ?', region.id).order('name').collect {|other_region| "<li><a href='/#{other_region.name.downcase}'>#{other_region.full_name}</a></li>"}.join
+    html << "</ul>"
+
+    html.html_safe
   end
 end
