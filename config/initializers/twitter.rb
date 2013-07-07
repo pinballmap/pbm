24be8df24b7660016d6e05af91d8c50de2306cfb
Scott Wainstock <scott.wainstock@gmail.com>
2013-07-07 13:45:01 -0700

    remove trailing comma from twitter config, use upper-case env variables

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = ENV['twitter_key'],
-  config.consumer_secret = ENV['twitter_secret']
-  config.oauth_token = ENV['twitter_token']
-  config.oauth_token_secret = ENV['twitter_secret_token']
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
 end
