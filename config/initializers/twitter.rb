56ed68ee11ee09707f5edfe1bd2384f0328ffc3b
Scott Wainstock <scott.wainstock@gmail.com>
2013-07-20 09:42:54 -0700

    increase twitter timeout

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -3,4 +3,8 @@ Twitter.configure do |config|
   config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
   config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
   config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
+  config.connection_options = Twitter::Default::CONNECTION_OPTIONS.merge(:request => {
+    :open_timeout => 10,
+    :timeout => 15,
+  })
 end
