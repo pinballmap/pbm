ddbff3d2eb263d7f858b3d202171ceb21bb1076c
Scott Wainstock <scott.wainstock@gmail.com>
2013-08-17 08:21:42 -0700

    Don't send error emails for MissingTemplate exceptions. We were getting flooded with scans and using up our SendGrid credits.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :detect_region, :set_current_user
 
+  rescue_from ActionView::MissingTemplate do |exception|
+  end
+
   protected
     def detect_region
       @region = Region.find_by_name(params[:region].downcase) if (params[:region] && (params[:region].is_a? String))
