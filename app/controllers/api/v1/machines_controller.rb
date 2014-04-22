380fa242c08df0126c7f84d01fcca25567f6785a
Scott Wainstock <ssw@rentrak.com>
2014-04-22 11:52:38 -0700

    api/v1/machines.json endpoint

--- app/controllers/api/v1/machines_controller.rb
+++ app/controllers/api/v1/machines_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class MachinesController < InheritedResources::Base
 
-      respond_to :xml
+      respond_to :xml, :json
 
       def index
         respond_with Machine.all
