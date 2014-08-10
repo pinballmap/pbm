680406b3e0eb1e38b67653a4362a238665f86f79
Scott Wainstock <ssw@rentrak.com>
2014-08-10 10:13:44 -0700

    add simplecov and coveralls support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,13 @@ require 'factory_girl_rails'
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
+require 'simplecov'
+require 'coveralls'
 
 include Sprockets::Rails::Helper
+
+SimpleCov.start 'rails'
+Coveralls.wear! 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
