02b1200a785dce5295feceb50066045b5c448ee9
Scott Wainstock <scott.wainstock@gmail.com>
2014-09-20 17:22:19 -0700

    report ip address when removing a machine from the api

--- spec/requests/api/v1/location_machine_xrefs_controller_spec.rb
+++ spec/requests/api/v1/location_machine_xrefs_controller_spec.rb
@@ -21,7 +21,7 @@ describe Api::V1::LocationMachineXrefsController, type: :request do
     it 'sends a deletion email when appropriate' do
       expect(Pony).to receive(:mail) do |mail|
         expect(mail).to include(
-          body: "#{@location.name}\n#{@machine.name}\n#{@location.region.name}\n(entered from )",
+          body: "#{@location.name}\n#{@machine.name}\n#{@location.region.name}\n(entered from 127.0.0.1)",
           subject: 'PBM - Someone removed a machine from a location',
           to: [],
           from: 'admin@pinballmap.com'
