8784f8ed68a4a943b0981eb0e8d5a0cc28bc6886
Scott Wainstock <scott.wainstock@gmail.com>
2015-02-01 14:41:42 -0800

    machine condition unit tests, rubocop compliancy

--- app/models/machine_condition.rb
+++ app/models/machine_condition.rb
@@ -1,6 +1,5 @@
 class MachineCondition < ActiveRecord::Base
-   belongs_to :location_machine_xref
+  belongs_to :location_machine_xref
 
-   attr_accessible :comment,:location_machine_xref
-
+  attr_accessible :comment, :location_machine_xref
 end
