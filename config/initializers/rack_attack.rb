4ca52d97cd3d1b04c19271cc4b3e76fa59c54fa5
Scott Wainstock <ssw@rentrak.com>
2015-09-05 09:20:37 -0700

    throttle connections in rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,11 +1,17 @@
-Rack::Attack.blacklist('block admin identified IPs') do |req|
-  should_ban = nil
+if Rails.env.production?
+    Rack::Attack.blacklist('block admin identified IPs') do |req|
+      should_ban = nil
 
-  BannedIp.all.each do |banned_ip|
-    if (banned_ip.ip_address == req.ip)
-      should_ban = 1
+      BannedIp.all.each do |banned_ip|
+        if (banned_ip.ip_address == req.ip)
+          should_ban = 1
+        end
+      end
+
+      should_ban
     end
-  end
 
-  should_ban
+    Rack::Attack.throttle('req/ip', :limit => 10, :period => 1.second) do |req|
+      req.ip
+    end
 end
