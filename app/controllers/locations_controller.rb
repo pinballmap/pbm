c715244a719c5223b49890fb04bae941bfcafbd7
Scott Wainstock <ssw@rentrak.com>
2015-09-26 16:56:28 -0700

    location and machine autocomplete searches populate dropdown

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,7 @@ class LocationsController < InheritedResources::Base
   has_scope :by_location_name, :by_location_id, :by_machine_id, :by_machine_name, :by_city_id, :by_machine_group_id, :by_zone_id, :by_operator_id, :by_type_id, :by_at_least_n_machines_city, :by_at_least_n_machines_zone, :by_at_least_n_machines_type, :region
 
   def autocomplete
-    render json: @region.locations.map { |l| l.name }.grep(/#{Regexp.escape params[:term] || ''}/i).sort
+    render json: @region.locations.select { |l| l.name =~ /#{Regexp.escape params[:term] || ''}/i }.sort_by(&:name).map { |l| { label: l.name, value: l.name, id: l.id } }
   end
 
   def index
