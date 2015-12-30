c30831c09b3a78b29edf3f8c5899be11ed18201c
Scott Wainstock <ssw@rentrak.com>
2015-12-30 15:05:44 -0800

    tests for auth required webpage logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102053730) do
+ActiveRecord::Schema.define(version: 20151230222502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "banned_ips", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "ip_address"
   end
 
@@ -167,13 +167,13 @@ ActiveRecord::Schema.define(version: 20151102053730) do
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month",      limit: 2
+    t.integer  "month"
     t.integer  "year",       limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_histories_on_item_and_table_and_month_and_year", using: :btree
 
   create_table "region_link_xrefs", force: true do |t|
     t.string  "name"
@@ -206,14 +206,19 @@ ActiveRecord::Schema.define(version: 20151102053730) do
     t.integer  "region_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "user_submissions", ["region_id"], name: "index_user_submissions_on_region_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "encrypted_password"
-    t.integer  "sign_in_count"
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",       limit: 128, default: "", null: false
+    t.string   "password_salt",                        default: "", null: false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                        default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -230,6 +235,7 @@ ActiveRecord::Schema.define(version: 20151102053730) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["region_id"], name: "index_users_on_region_id", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "zones", force: true do |t|
     t.string   "name"
