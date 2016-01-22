59c923b7d1f4f2b25a0590165c0eba2110d5c04c
Scott Wainstock <ssw@rentrak.com>
2016-01-22 15:01:48 -0800

    fix super admin location region update bug

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,81 +4,12 @@
 RailsAdmin.config do |config|
   config.authorize_with :cancan
 
-  # If your default_local is different from :en, uncomment the following 2 lines and set your default locale here:
-  # require 'i18n'
-  # I18n.default_locale = :de
-
   config.current_user_method(&:current_user)
 
-  # If you want to track changes on your models:
   config.audit_with :history, User
 
-  # Or with a PaperTrail: (you need to install it first)
-  # config.audit_with :paper_trail, User
-
-  # Set the admin name here (optional second array element will appear in a beautiful RailsAdmin red ©)
   config.main_app_name = ['Pinball Map', 'Admin']
-  # or for a dynamic name:
-  # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
-
-
-  #  ==> Global show view settings
-  # Display empty fields in show views
-  # config.compact_show_view = false
-
-  #  ==> Global list view settings
-  # Number of default rows per-page:
-  # config.default_items_per_page = 20
-
-  #  ==> Included models
-  # Add all excluded models here:
   config.excluded_models = []
-
-  # Add models here if you want to go 'whitelist mode':
-  # config.included_models = [Event, Location, LocationMachineXref, LocationPictureXref, LocationType, Machine, Operator, Region, RegionLinkXref, Zone]
-
-  # Application wide tried label methods for models' instances
-  # config.label_methods << :description # Default is [:name, :title]
-
-  #  ==> Global models configuration
-  # config.models do
-  #   # Configuration here will affect all included models in all scopes, handle with care!
-  #
-  #   list do
-  #     # Configuration here will affect all included models in list sections (same for show, export, edit, update, create)
-  #
-  #     fields_of_type :date do
-  #       # Configuration here will affect all date fields, in the list section, for all included models. See README for a comprehensive type list.
-  #     end
-  #   end
-  # end
-  #
-  #  ==> Model specific configuration
-  # Keep in mind that *all* configuration blocks are optional.
-  # RailsAdmin will try his best to provide the best defaults for each section, for each field.
-  # Try to override as few things as possible, in the most generic way. Try to avoid setting labels for models and attributes, use ActiveRecord I18n API instead.
-  # Less code is better code!
-  # config.model MyModel do
-  #   # Cross-section field configuration
-  #   object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
-  #   label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
-  #   label_plural 'My models'      # Same, plural
-  #   weight -1                     # Navigation priority. Bigger is higher.
-  #   parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
-  #   navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
-  #   # Section specific configuration:
-  #   list do
-  #     filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
-  #     items_per_page 100    # Override default_items_per_page
-  #     sort_by :id           # Sort column (default is primary key)
-  #     sort_reverse true     # Sort direction (default is true for primary key, last created first)
-  #     # Here goes the fields configuration for the list view
-  #   end
-  # end
-
-  # Your model's configuration, to help you get started:
-
-  # All fields marked as 'hidden' won't be shown anywhere in the rails_admin unless you mark them as visible. (visible(true))
 
   config.model Event do
     list do
@@ -221,7 +152,7 @@ RailsAdmin.config do |config|
       field :description, :string 
       field :region_id do
         render do
-          bindings[:view].render :partial => 'region_edit', :locals => {:region_id => Authorization.current_user.region_id, :object_type => 'location'}
+          bindings[:view].render :partial => 'region_edit', :locals => {:region_id => Authorization.current_user.is_super_admin ? nil : Authorization.current_user.region_id, :object_type => 'location'}
         end
       end
     end
