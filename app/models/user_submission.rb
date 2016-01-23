9693df7e7b2f81079f3fa7e4c7180fb9ab6c21bb
Scott Wainstock <ssw@rentrak.com>
2016-01-23 13:05:21 -0800

    user submission remove machine api endpoint

--- app/models/user_submission.rb
+++ app/models/user_submission.rb
@@ -3,6 +3,8 @@ class UserSubmission < ActiveRecord::Base
 
   attr_accessible :region_id, :submission_type, :submission
 
+  scope :region, ->(name) { where(region_id: Region.find_by_name(name.downcase).id) }
+
   SUGGEST_LOCATION_TYPE = 'suggest_location'
   REMOVE_MACHINE_TYPE = 'remove_machine'
   CONTACT_US_TYPE = 'contact_us'
---
+++ spec/requests/api/v1/user_submissions_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Api::V1::UserSubmissionsController, type: :request do
+  before(:each) do
+    @region = FactoryGirl.create(:region, name: 'portland')
+  end
+
+  describe '#index' do
+    it 'returns all submissions within scope' do
+      FactoryGirl.create(:user_submission, region: @region, submission_type: 'remove_machine', submission: 'foo')
+      FactoryGirl.create(:user_submission, region: FactoryGirl.create(:region, name: 'chicago'), submission_type: 'remove_machine', submission: 'foo')
+      get "/api/v1/region/#{@region.name}/user_submissions.json"
+
+      expect(response.body).to include('remove_machine')
+      expect(response.body).to include('foo')
+
+      expect(response.body).to_not include('bar')
+    end
+
+    it 'only shows remove_machine submissions' do
+      FactoryGirl.create(:user_submission, region: @region, submission_type: 'remove_machine', submission: 'removed foo from bar')
+      FactoryGirl.create(:user_submission, region: @region, submission_type: 'DO_NOT_SHOW', submission: 'hope this does not show')
+      get "/api/v1/region/#{@region.name}/user_submissions.json"
+
+      expect(response.body).to include('remove_machine')
+      expect(response.body).to include('removed foo from bar')
+
+      expect(response.body).to_not include('DO_NOT_SHOW')
+      expect(response.body).to_not include('hope this does not show')
+    end
+  end
+
+end
