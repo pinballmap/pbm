fe2df161ddbf403c04865300a3d3e332499c016f
Scott Wainstock <scott.wainstock@gmail.com>
2016-04-24 20:06:02 -0700

    is disabled for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403220735) do
+ActiveRecord::Schema.define(version: 20160425025650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -231,6 +231,7 @@ ActiveRecord::Schema.define(version: 20160403220735) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.boolean  "is_disabled"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
