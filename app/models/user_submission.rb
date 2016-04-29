551f1971960cba873527ba23a798441d3440db58
Scott Wainstock <scott.wainstock@gmail.com>
2016-04-28 22:02:18 -0700

    include username and email in email alerts

--- app/models/user_submission.rb
+++ app/models/user_submission.rb
@@ -2,7 +2,7 @@ class UserSubmission < ActiveRecord::Base
   belongs_to :region
   belongs_to :user
 
-  attr_accessible :region_id, :user_id, :submission_type, :submission
+  attr_accessible :region_id, :user, :user_id, :submission_type, :submission
 
   scope :region, ->(name) { where(region_id: Region.find_by_name(name.downcase).id) }
 
