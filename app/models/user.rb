95e6ee146720549fa659add82deadd342be45005
Scott Wainstock <scott.wainstock@gmail.com>
2016-05-01 12:53:23 -0700

    username max length to 15

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
-  validates :username, length: { maximum: 6 }
+  validates :username, length: { maximum: 15 }
 
   validate :validate_username
 
