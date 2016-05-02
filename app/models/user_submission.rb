c04a8b9a9f6dd092f0ac1b76d1eca7545f5a8893
Scott Wainstock <scott.wainstock@gmail.com>
2016-05-01 22:18:12 -0700

    make user_submission when a new machine_condition is created

--- app/models/user_submission.rb
+++ app/models/user_submission.rb
@@ -6,10 +6,11 @@ class UserSubmission < ActiveRecord::Base
 
   scope :region, ->(name) { where(region_id: Region.find_by_name(name.downcase).id) }
 
-  SUGGEST_LOCATION_TYPE = 'suggest_location'
-  REMOVE_MACHINE_TYPE = 'remove_machine'
-  CONTACT_US_TYPE = 'contact_us'
   NEW_LMX_TYPE = 'new_lmx'
+  CONTACT_US_TYPE = 'contact_us'
+  NEW_CONDITION_TYPE = 'new_condition'
+  REMOVE_MACHINE_TYPE = 'remove_machine'
+  SUGGEST_LOCATION_TYPE = 'suggest_location'
 
   def user_email
     user ? user.email : ''
