142077bbe07974c0e0c555fd5ee92a52f82c172d
Scott Wainstock <scott.wainstock@gmail.com>
2016-05-28 21:29:49 -0700

    only send one id param, login, for user auth_details

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,21 +6,15 @@ module Api
 
       api :GET, '/api/v1/users/auth_details.json', 'Fetch auth info for a user'
       description "This info includes the user's API token."
-      param :user_email, String, desc: "User's email address -- this field or username are required", required: false
-      param :username, String, desc: "User's username -- this field or username are required", required: false
+      param :login, String, desc: "User's username or email address", required: true
       param :password, String, desc: "User's password", required: true
       def auth_details
-        if (params[:user_email].blank? && params[:username].blank?) || params[:password].blank?
-          return_response('(username or user_email) and password are required fields', 'errors')
+        if params[:login].blank? || params[:password].blank?
+          return_response('login and password are required fields', 'errors')
           return
         end
 
-        user = nil
-        if params[:user_email]
-          user = User.find_by_email(params[:user_email])
-        else
-          user = User.find_by_username(params[:username])
-        end
+        user = User.find_by_username(params[:login]) || User.find_by_email(params[:login])
 
         unless user
           return_response('Unknown user', 'errors')
