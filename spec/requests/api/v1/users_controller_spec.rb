c783421ca3c990a5c575a1f54aa71617c3a19cda
Scott Wainstock <scott.wainstock@gmail.com>
2016-05-28 21:29:49 -0700

    only send one id param, login, for user auth_details

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -7,14 +7,14 @@ describe Api::V1::UsersController, type: :request do
 
   describe '#auth_details' do
     it 'returns all app-centric user data' do
-      get '/api/v1/users/auth_details.json', user_email: 'yeah@ok.com', password: 'okokok'
+      get '/api/v1/users/auth_details.json', login: 'yeah@ok.com', password: 'okokok'
 
       expect(response).to be_success
       expect(response.body).to include('ssw')
       expect(response.body).to include('yeah@ok.com')
       expect(response.body).to include('abc123')
 
-      get '/api/v1/users/auth_details.json', username: 'ssw', password: 'okokok'
+      get '/api/v1/users/auth_details.json', login: 'ssw', password: 'okokok'
 
       expect(response).to be_success
       expect(response.body).to include('ssw')
@@ -26,23 +26,23 @@ describe Api::V1::UsersController, type: :request do
       get '/api/v1/users/auth_details.json', password: 'okokok'
 
       expect(response).to be_success
-      expect(JSON.parse(response.body)['errors']).to eq('(username or user_email) and password are required fields')
+      expect(JSON.parse(response.body)['errors']).to eq('login and password are required fields')
 
-      get '/api/v1/users/auth_details.json', username: 'ssw'
+      get '/api/v1/users/auth_details.json', login: 'ssw'
 
       expect(response).to be_success
-      expect(JSON.parse(response.body)['errors']).to eq('(username or user_email) and password are required fields')
+      expect(JSON.parse(response.body)['errors']).to eq('login and password are required fields')
     end
 
     it 'tells you if you enter the wrong password' do
-      get '/api/v1/users/auth_details.json', username: 'ssw', password: 'NOT_okokok'
+      get '/api/v1/users/auth_details.json', login: 'ssw', password: 'NOT_okokok'
 
       expect(response).to be_success
       expect(JSON.parse(response.body)['errors']).to eq('Incorrect password')
     end
 
     it 'tells you if this user does not exist' do
-      get '/api/v1/users/auth_details.json', username: 's', password: 'okokok'
+      get '/api/v1/users/auth_details.json', login: 's', password: 'okokok'
 
       expect(response).to be_success
       expect(JSON.parse(response.body)['errors']).to eq('Unknown user')
