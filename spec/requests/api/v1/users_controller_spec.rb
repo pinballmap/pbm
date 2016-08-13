15fcaa98b8fbf76a158951021234e0b498d2df18
Scott Wainstock <scott.wainstock@gmail.com>
2016-08-13 14:25:43 -0700

    signup api endpoint

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController, type: :request do
-  before(:each) do
-    FactoryGirl.create(:user, id: 1, username: 'ssw', email: 'yeah@ok.com', password: 'okokok', password_confirmation: 'okokok', authentication_token: 'abc123')
-  end
-
   describe '#auth_details' do
+    before(:each) do
+      FactoryGirl.create(:user, id: 1, username: 'ssw', email: 'yeah@ok.com', password: 'okokok', password_confirmation: 'okokok', authentication_token: 'abc123')
+    end
+
     it 'returns all app-centric user data' do
       get '/api/v1/users/auth_details.json', login: 'yeah@ok.com', password: 'okokok'
 
@@ -46,6 +46,61 @@ describe Api::V1::UsersController, type: :request do
 
       expect(response).to be_success
       expect(JSON.parse(response.body)['errors']).to eq('Unknown user')
+    end
+  end
+
+  describe '#signup' do
+    it 'returns all app-centric user data if successful' do
+      post '/api/v1/users/signup.json', username: 'foo', email: 'yeah@ok.com', password: 'okokok', confirm_password: 'okokok'
+
+      expect(response).to be_success
+      expect(response.body).to include('foo')
+      expect(response.body).to include('yeah@ok.com')
+      expect(response.body).to include('authentication_token')
+    end
+
+    it 'requires a username and email address' do
+      post '/api/v1/users/signup.json', username: '', email: 'yeah@ok.com', password: 'okokok', confirm_password: 'okokok'
+
+      expect(response).to be_success
+      expect(JSON.parse(response.body)['errors']).to eq('username and email are required fields')
+
+      post '/api/v1/users/signup.json', username: 'yeah', email: '', password: 'okokok', confirm_password: 'okokok'
+
+      expect(response).to be_success
+      expect(JSON.parse(response.body)['errors']).to eq('username and email are required fields')
+    end
+
+    it 'does not allow blank passwords' do
+      post '/api/v1/users/signup.json', username: 'yeah', email: 'yeah@ok.com', password: '', confirm_password: ''
+
+      expect(response).to be_success
+      expect(JSON.parse(response.body)['errors']).to eq('password can not be blank')
+    end
+
+    it 'tells you if passwords do not match' do
+      post '/api/v1/users/signup.json', username: 'yeah', email: 'yeah@ok.com', password: 'okokok', confirm_password: 'NOPE'
+
+      expect(response).to be_success
+      expect(JSON.parse(response.body)['errors']).to eq('your entered passwords do not match')
+    end
+
+    it 'does not allow duplicated usernames' do
+      FactoryGirl.create(:user, id: 1, username: 'ssw', email: 'yeah@ok.com', password: 'okokok', password_confirmation: 'okokok', authentication_token: 'abc123')
+
+      post '/api/v1/users/signup.json', username: 'ssw', email: 'yeah@ok.com', password: 'okokok', confirm_password: 'okokok'
+
+      expect(response).to be_success
+      expect(JSON.parse(response.body)['errors']).to eq('This username already exists')
+    end
+
+    it 'does not allow duplicated email addresses' do
+      FactoryGirl.create(:user, id: 1, username: 'ssw', email: 'yeah@ok.com', password: 'okokok', password_confirmation: 'okokok', authentication_token: 'abc123')
+
+      post '/api/v1/users/signup.json', username: 'CLEO', email: 'yeah@ok.com', password: 'okokok', confirm_password: 'okokok'
+
+      expect(response).to be_success
+      expect(JSON.parse(response.body)['errors']).to eq('This email address already exists')
     end
   end
 end
