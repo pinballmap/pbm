bfe85386bbd42952ee8088d248e7b71b4dadfa68
ryantg <gratzer@posteo.org>
2016-11-07 09:48:49 -0800

    changed burlington region to vermont, added redirect from old url

--- config/routes.rb
+++ config/routes.rb
@@ -137,6 +137,7 @@ Pbm::Application.routes.draw do
 
   # legacy names for regions
   get '/milwaukee' => redirect('/wisconsin')
+  get '/burlington' => redirect('/vermont')
 
   root to: 'pages#home'
 end
