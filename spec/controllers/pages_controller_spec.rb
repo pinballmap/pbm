139274d120747044cb4729ba0a6f258f3f2afa92
ryantg <gratzer@posteo.org>
2017-02-20 18:35:27 -0800

    fixed a test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -159,7 +159,7 @@ HERE
         )
       end
 
-      post 'submitted_new_location', region: 'portland', location_name: 'name', location_street: 'street', location_city: 'city', location_state: 'state', location_zip: 'zip', location_phone: 'phone', location_website: 'website', location_operator: 'operator', location_machines: 'machines', submitter_name: 'subname', submitter_email: 'subemail'
+      post '_location', region: 'portland', location_name: 'name', location_street: 'street', location_city: 'city', location_state: 'state', location_zip: 'zip', location_phone: 'phone', location_website: 'website', location_operator: 'operator', location_machines: 'machines', submitter_name: 'subname', submitter_email: 'subemail'
 
       expect(@region.user_submissions.count).to eq(1)
       submission = @region.user_submissions.first
@@ -171,7 +171,7 @@ HERE
       login(FactoryGirl.create(:user, username: 'ssw', email: 'yeah@ok.com'))
 
       body = <<HERE
-(A new pinball spot has been submitted for your region! Please verify the address on http://maps.google.com and then paste that Google Maps address into http://pinballmap.com/admin. Thanks!)\n
+(A new pinball spot has been submitted for your region! Please verify the address on https://maps.google.com and then paste that Google Maps address into https://pinballmap.com/admin. Thanks!)\n
 Location Name: name\n
 Street: street\n
 City: city\n
