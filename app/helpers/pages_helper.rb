22929c7f0e761c6c5faa06bf68110d1c77b6f67f
ryantg <gratzer@posteo.org>
2017-03-06 19:31:14 -0800

    dynamic page descriptions

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -56,4 +56,56 @@ module PagesHelper
     title += 'Pinball Map'
     title
   end
+
+  def desc_for_path(path, region = nil)
+    desc = region.nil? ? desc_for_regionless_path(path) : desc_for_region_path(path, region)
+    desc
+  end
+
+  def desc_for_region_path(path, region)
+    desc = ''
+    if path == suggest_path(region.name)
+      desc = "Add a new location to the #{region.full_name} Pinball Map! This crowdsourced map relies on your knowledge and help!"
+    elsif path == about_path(region.name)
+      desc = "Contact the administrator of the #{region.full_name} Pinball Map. Suggest a new region. Check out the most popular pinball machines on the map!"
+    elsif path == events_path(region.name)
+      desc = "Upcoming pinball events in #{region.full_name}. Tournaments, leagues, charities, launch parties, and more!"
+    elsif path == high_rollers_path(region.name)
+      desc = "High scores for the #{region.full_name} Pinball Map! If you get a high score on a pinball machine, add it to the map!"
+    else
+      desc = "Find local places to play pinball! The #{region.full_name} Pinball Map is a high-quality user-updated pinball locator for all the public pinball machines in your area."
+    end
+
+    desc
+  end
+
+  def desc_for_regionless_path(path)
+    desc = ''
+
+    if path == app_path
+      desc = "Pinball Map App for iOS and Android. Find pinball machines to play near you! Update the app like the true champ you are."
+    elsif path == app_support_path
+      desc = 'Pinball Map iOS screenshots, support, and FAQ'
+    elsif path == faq_path
+      desc = 'Pinball Map Frequently Asked Questions (FAQ). Got a question? It may be answered here.'
+    elsif path == store_path
+      desc = "Pinball Map Store! We have a t-shirt for sale. Get it while it's hot (pink)."
+    elsif path == donate_path
+      desc = "Donate to Pinball Map. Donations help us manage the costs of running the site. Thank you!"
+    elsif path =~ /profile/
+      desc = "The user profile tracks your Pinball Map contributions. It's a concise overview of your edits, high scores, and favorite locations."
+    elsif path =~ /login/
+      desc = 'Log in to Pinball Map and help keep it up to date!'
+    elsif path =~ /join/
+      desc = "Join Pinball Map and help keep your local pinball map up to date! This site relies on user contributions. Joining is quick and easy!"
+    elsif path =~ /password/
+      desc = 'If you forgot your Pinball Map password, you can recover it from here.'
+    elsif path =~ /confirmation/
+      desc = "The email confirmation can be resent from this page."
+    else
+      desc = "The Pinball Map website and free mobile app will help you places to play pinball! Pinball Map is a high-quality user-updated pinball locator for all the public pinball machines in your area."
+    end
+
+    desc
+  end
 end
