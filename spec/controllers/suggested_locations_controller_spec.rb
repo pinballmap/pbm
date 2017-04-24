cfacca2f081ce8c67b1d9d4f79aad5e9d40a107a
Scott Wainstock <scott.wainstock@gmail.com>
2017-04-23 20:39:02 -0700

    add machines from suggested locations when possible

--- spec/controllers/suggested_locations_controller_spec.rb
+++ spec/controllers/suggested_locations_controller_spec.rb
@@ -8,11 +8,17 @@ describe SuggestedLocationsController, type: :controller do
     @lt = FactoryGirl.create(:location_type, name: 'lt')
     @o = FactoryGirl.create(:operator, name: 'o')
 
-    @sl = FactoryGirl.create(:suggested_location, name: 'name', street: 'street', city: 'city', state: 'OR', zip: '97203', phone: '555-555-5555', lat: 11.11, lon: 22.22, website: 'http://www.cool.com', region: @r, location_type: @lt, operator: @o)
+    @sl = FactoryGirl.create(:suggested_location, name: 'name', street: 'street', city: 'city', state: 'OR', zip: '97203', phone: '555-555-5555', lat: 11.11, lon: 22.22, website: 'http://www.cool.com', region: @r, location_type: @lt, operator: @o, machines: 'The Dark Knight (Stern, 2008), Challenger (Gottlieb, 1971), this will not match')
   end
 
   describe '#convert_to_location' do
     it 'should create a corresponding location, delete itself, redirect to admin page' do
+      m_one = FactoryGirl.create(:machine, name: 'The Dark Knight', manufacturer: 'Stern', year: '2008')
+      m_two = FactoryGirl.create(:machine, name: 'Challenger', manufacturer: 'Gottlieb', year: '1971')
+
+      FactoryGirl.create(:machine, name: 'Challenger', manufacturer: 'Stern', year: '1971')
+      FactoryGirl.create(:machine, name: 'Challenger', manufacturer: 'Gottlieb', year: '2222')
+
       post :convert_to_location, format: :json, id: @sl.id
 
       l = Location.find_by_name('name')
@@ -30,6 +36,12 @@ describe SuggestedLocationsController, type: :controller do
       expect(l.operator).to eq(@o)
 
       expect(SuggestedLocation.all.size).to eq(0)
+
+      (lmx_one, lmx_two) = LocationMachineXref.all
+      expect(lmx_one.location).to eq(l)
+      expect(lmx_one.machine).to eq(m_one)
+      expect(lmx_two.location).to eq(l)
+      expect(lmx_two.machine).to eq(m_two)
 
       expect(response).to redirect_to('/admin')
     end
