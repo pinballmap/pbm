dc784624dba81b9e51d83de517ae1d350b9cdc46
Scott Wainstock <scott.wainstock@gmail.com>
2017-04-24 21:09:20 -0700

    skip geocoding suggested location if test

--- app/models/suggested_location.rb
+++ app/models/suggested_location.rb
@@ -4,7 +4,7 @@ class SuggestedLocation < ActiveRecord::Base
   belongs_to :location_type
 
   geocoded_by :full_street_address, latitude: :lat, longitude: :lon
-  before_validation :geocode
+  before_validation :geocode, unless: ENV['SKIP_GEOCODE']
 
   attr_accessible :name, :street, :city, :state, :zip, :phone, :lat, :lon, :website, :region_id, :location_type_id, :comments, :operator_id, :machines, :operator, :location_type, :user_inputted_address
 
