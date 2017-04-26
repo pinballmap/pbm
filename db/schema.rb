1f7d0deda497d6ba71f7452087a3198d44e9bf1b
ryantg <gratzer@posteo.org>
2017-04-25 21:32:18 -0700

    merged schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20170418033559) do
   enable_extension "plpgsql"
 
   create_table "banned_ips", force: :cascade do |t|
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "ip_address", limit: 255
   end
 
@@ -168,13 +168,13 @@ ActiveRecord::Schema.define(version: 20170418033559) do
     t.string   "username",   limit: 255
     t.integer  "item"
     t.string   "table",      limit: 255
-    t.integer  "month"
+    t.integer  "month",      limit: 2
     t.integer  "year",       limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_histories_on_item_and_table_and_month_and_year", using: :btree
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
   create_table "region_link_xrefs", force: :cascade do |t|
     t.string  "name",        limit: 255
@@ -201,19 +201,6 @@ ActiveRecord::Schema.define(version: 20170418033559) do
     t.boolean  "should_auto_delete_empty_locations"
     t.boolean  "send_digest_comment_emails"
     t.boolean  "send_digest_removal_emails"
-  end
-
-  create_table "ssw_tmp_weird_empty_lmxes", id: false, force: :cascade do |t|
-    t.integer  "id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "location_id"
-    t.integer  "machine_id"
-    t.text     "condition"
-    t.date     "condition_date"
-    t.string   "ip",                        limit: 255
-    t.integer  "user_id"
-    t.integer  "machine_score_xrefs_count"
   end
 
   create_table "suggested_locations", force: :cascade do |t|
@@ -248,13 +235,9 @@ ActiveRecord::Schema.define(version: 20170418033559) do
   add_index "user_submissions", ["region_id"], name: "index_user_submissions_on_region_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                    limit: 255, default: "", null: false
-    t.string   "encrypted_password",       limit: 128, default: "", null: false
-    t.string   "password_salt",            limit: 255, default: "", null: false
-    t.string   "reset_password_token",     limit: 255
-    t.string   "remember_token",           limit: 255
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        default: 0
+    t.string   "email",                    limit: 255
+    t.string   "encrypted_password",       limit: 255
+    t.integer  "sign_in_count"
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",       limit: 255
@@ -278,7 +261,6 @@ ActiveRecord::Schema.define(version: 20170418033559) do
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["region_id"], name: "index_users_on_region_id", using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "zones", force: :cascade do |t|
