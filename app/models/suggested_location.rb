f00955bb4eda9163c4b02df2d57d75634890f0bd
ryantg <gratzer@posteo.org>
2017-05-01 20:51:42 -0700

    revert suggested location form validation until I can figure out test

--- app/models/suggested_location.rb
+++ app/models/suggested_location.rb
@@ -1,10 +1,4 @@
 class SuggestedLocation < ActiveRecord::Base
-  validates_presence_of :name, :street, :city, :state, :zip
-  validates :phone, format: { with: /\A(\(\d{3}\) |\d{3}-)\d{3}-\d{4}\z/, message: 'format invalid, please use ###-###-#### or (###) ###-####' }, if: :phone?
-  validates :website, format: { with: %r{^http[s]?:\/\/}, message: 'must begin with http:// or https://', multiline: true }, if: :website?
-  validates :name, :street, :city, :state, format: { with: /^\S.*/, message: "Can't start with a blank", multiline: true }
-  validates :lat, :lon, presence: { message: 'Latitude/Longitude failed to generate. Please double check address and try again, or manually enter the lat/lon' }
-
   belongs_to :region
   belongs_to :operator
   belongs_to :location_type
