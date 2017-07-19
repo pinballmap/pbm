7a8e9316d965e57cf30a3685f033d03f8244dbf9
Scott Wainstock <scott.wainstock@gmail.com>
2017-07-19 14:01:11 -0700

    allow extra () machine names in suggested location promotion

--- spec/controllers/suggested_locations_controller_spec.rb
+++ spec/controllers/suggested_locations_controller_spec.rb
@@ -8,13 +8,14 @@ describe SuggestedLocationsController, type: :controller do
     @lt = FactoryGirl.create(:location_type, name: 'lt')
     @o = FactoryGirl.create(:operator, name: 'o')
 
-    @sl = FactoryGirl.create(:suggested_location, name: 'name', street: 'street', city: 'city', state: 'OR', zip: '97203', phone: '555-555-5555', lat: 11.11, lon: 22.22, website: 'http://www.cool.com', region: @r, location_type: @lt, operator: @o, machines: 'The Dark Knight (Stern, 2008), Challenger [Gottlieb - 1971], this will not match')
+    @sl = FactoryGirl.create(:suggested_location, name: 'name', street: 'street', city: 'city', state: 'OR', zip: '97203', phone: '555-555-5555', lat: 11.11, lon: 22.22, website: 'http://www.cool.com', region: @r, location_type: @lt, operator: @o, machines: 'The Dark Knight (Stern, 2008), Star Trek (Pro) (Stern, 2013), Challenger [Gottlieb - 1971], this will not match')
   end
 
   describe '#convert_to_location' do
     it 'should create a corresponding location, delete itself, redirect to admin page' do
       m_one = FactoryGirl.create(:machine, name: 'The Dark Knight', manufacturer: 'Stern', year: '2008')
       m_two = FactoryGirl.create(:machine, name: 'Challenger', manufacturer: 'Gottlieb', year: '1971')
+      m_three = FactoryGirl.create(:machine, name: 'Star Trek (Pro)', manufacturer: 'Stern', year: '2013')
 
       FactoryGirl.create(:machine, name: 'Challenger', manufacturer: 'Stern', year: '1971')
       FactoryGirl.create(:machine, name: 'Challenger', manufacturer: 'Gottlieb', year: '2222')
@@ -37,11 +38,13 @@ describe SuggestedLocationsController, type: :controller do
 
       expect(SuggestedLocation.all.size).to eq(0)
 
-      (lmx_one, lmx_two) = LocationMachineXref.all
+      (lmx_one, lmx_two, lmx_three) = LocationMachineXref.all
       expect(lmx_one.location).to eq(l)
       expect(lmx_one.machine).to eq(m_one)
       expect(lmx_two.location).to eq(l)
-      expect(lmx_two.machine).to eq(m_two)
+      expect(lmx_two.machine).to eq(m_three)
+      expect(lmx_three.location).to eq(l)
+      expect(lmx_three.machine).to eq(m_two)
 
       expect(response).to redirect_to('/admin')
     end
