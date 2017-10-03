3d19d6a0c293b4f8e854487ed2a5b2cc8c49d195
ryantg <gratzer@posteo.org>
2017-10-02 22:47:50 -0700

    removed closed_arrow open_arrow

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,16 +1,9 @@
 module LocationsHelper
-  def open_closed_arrows_for(name, id = nil)
-    arrows = "<div class='arrow' id='#{name}_open_arrow#{"_#{id}" if id}' style='display: none;'><img alt='open arrow' src='#{asset_path 'open_arrow.gif'}' /></div>"
-    arrows += "<div class='arrow' id='#{name}_closed_arrow#{"_#{id}" if id}'><img alt='closed arrow' src='#{asset_path 'closed_arrow.gif'}' /></div>"
-
-    arrows.html_safe
-  end
 
   def banner(type, header_text, obj = nil)
     html = <<HERE
   <div id="#{type}_banner#{"_#{obj.id}" if obj}" class="sub_nav_item #{type}_toggle" onclick="toggleData('#{type}'#{", #{obj.id}" if obj});">
     <span>#{header_text}</span>
-    #{open_closed_arrows_for(type, obj ? obj.id : nil)}
   </div>
 HERE
     html.html_safe
--- spec/helpers/locations_helper_spec.rb
+++ spec/helpers/locations_helper_spec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe LocationsHelper, type: :helper do
-  describe '#open_closed_arrows' do
-    it 'should give me some open and closed arrows' do
-      l = FactoryGirl.create(:location)
-      expect(helper.open_closed_arrows_for('foo', l.id)).to match("<div class='arrow' id='foo_open_arrow_#{l.id}' style='display: none;'><img alt='open arrow' src='/assets/open_arrow.*.gif' /></div><div class='arrow' id='foo_closed_arrow_#{l.id}'><img alt='closed arrow' src='/assets/closed_arrow.*.gif' /></div>")
-
-      expect(helper.open_closed_arrows_for('foo')).to match("<div class='arrow' id='foo_open_arrow' style='display: none;'><img alt='open arrow' src='/assets/open_arrow.*.gif' /></div><div class='arrow' id='foo_closed_arrow'><img alt='closed arrow' src='/assets/closed_arrow.*.gif' /></div>")
-    end
-  end
 
   describe '#search_banner' do
     it 'should give me a banner' do
