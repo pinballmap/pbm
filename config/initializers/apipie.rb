b76a6b9f79496d9a2c1ae9916796ddd0f27e15ad
Scott Wainstock <scott.wainstock@gmail.com>
2018-04-17 20:08:48 -0700

    don't translate apieapie docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -4,6 +4,7 @@ Apipie.configure do |config|
   config.doc_base_url            = "/api/v1/docs"
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/v1/*.rb"
   config.validate                = false
+  config.translate               = false
   config.api_routes              = Rails.application.routes
   config.app_info = "
     If you use this API for something cool, please let us know, we like looking at interesting uses of the data.
