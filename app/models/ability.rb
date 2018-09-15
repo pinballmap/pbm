1b55f7d086bfe6589a607a86e29b9aa617d2e6aa
Scott Wainstock <scott.wainstock@gmail.com>
2018-09-15 13:10:48 -0700

    allow super admins to update region of operators (and set it to regionless)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,15 +7,15 @@ class Ability
     can :access, :rails_admin
     can :read, :dashboard
     can :history
-    can :manage, [Event, Operator, RegionLinkXref, Zone], region_id: user.region_id
+    can :manage, [Event, RegionLinkXref, Zone], region_id: user.region_id
     can %i[read], [UserSubmission], region_id: user.region_id
     can %i[update read destroy], [LocationPictureXref], location: { region_id: user.region_id }
     can %i[update read destroy], [MachineCondition, MachineScoreXref], location: { region_id: user.region_id }
 
     if user.is_super_admin
-      can :manage, [Location, User, UserSubmission, SuggestedLocation, LocationPictureXref]
+      can :manage, [Operator, Location, User, UserSubmission, SuggestedLocation, LocationPictureXref]
     else
-      can :manage, [Location], region_id: user.region_id
+      can :manage, [Location, Operator], region_id: user.region_id
       can %i[update read destroy], [SuggestedLocation], region_id: user.region_id
     end
 
