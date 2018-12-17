9a3d97b0024bbd3f7891bc8cf047e4105d1587cc
Scott Wainstock <scott.wainstock@gmail.com>
2018-12-17 10:51:33 -0800

    make name and machines required fields for website suggested locations

--- spec/models/suggested_location.rb
+++ spec/models/suggested_location.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SuggestedLocation do
   before(:each) do
-    @suggested_location = FactoryBot.create(:suggested_location, id: 1000, region: FactoryBot.create(:region, name: 'chicago'), lat: 1, lon: 2, name: 'foo', street: 'foo', state: 'OR', zip: '97203', city: 'Portland')
+    @suggested_location = FactoryBot.create(:suggested_location, id: 1000, region: FactoryBot.create(:region, name: 'chicago'), lat: 1, lon: 2, name: 'foo', street: 'foo', state: 'OR', zip: '97203', city: 'Portland', machines: 'Batman')
     @user = FactoryBot.create(:user, email: 'yeah@ok.com')
   end
 
@@ -10,28 +10,28 @@ describe SuggestedLocation do
     it 'should put http:// in front of websites without one' do
       expect(@suggested_location.website).to be(nil)
 
-      location_with_complete_website = FactoryBot.create(:suggested_location, website: 'http://foo.com')
+      location_with_complete_website = FactoryBot.create(:suggested_location, name: 'foo', machines: 'Batman', website: 'http://foo.com')
       expect(location_with_complete_website.website).to eq('http://foo.com')
 
-      location_with_incomplete_website = FactoryBot.create(:suggested_location, website: 'bar.com')
+      location_with_incomplete_website = FactoryBot.create(:suggested_location, name: 'foo', machines: 'Batman', website: 'bar.com')
       expect(location_with_incomplete_website.website).to eq('http://bar.com')
 
-      location_with_incomplete_website = FactoryBot.create(:suggested_location, website: '')
+      location_with_incomplete_website = FactoryBot.create(:suggested_location, name: 'foo', machines: 'Batman', website: '')
       expect(location_with_incomplete_website.website).to eq('')
 
       expect(@suggested_location.country).to eq('US')
 
-      filled_in_country = FactoryBot.create(:suggested_location, country: 'FR')
+      filled_in_country = FactoryBot.create(:suggested_location, name: 'foo', machines: 'Batman', country: 'FR')
       expect(filled_in_country.country).to eq('FR')
     end
 
     it 'should tag the location with US as the country if no country is sent' do
       expect(@suggested_location.website).to be(nil)
 
-      location_with_complete_website = FactoryBot.create(:suggested_location, website: 'http://foo.com')
+      location_with_complete_website = FactoryBot.create(:suggested_location, name: 'foo', machines: 'Batman', website: 'http://foo.com')
       expect(location_with_complete_website.website).to eq('http://foo.com')
 
-      location_with_incomplete_website = FactoryBot.create(:suggested_location, website: 'bar.com')
+      location_with_incomplete_website = FactoryBot.create(:suggested_location, name: 'foo', machines: 'Batman', website: 'bar.com')
       expect(location_with_incomplete_website.website).to eq('http://bar.com')
     end
   end
