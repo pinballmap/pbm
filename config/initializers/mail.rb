8f8165ad52d78e06b8c37119aba372dee37ab14e
ryantg <gratzer@fastmail.com>
2021-01-05 20:54:22 -0800

    use sendgrid api key environmental variable

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => 'apikey',
+  :password       => ENV['SENDGRID_API_KEY'],
   :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
