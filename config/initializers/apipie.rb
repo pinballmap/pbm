01f9b0b917b0db010d3a008b1940b83d27c7b13d
ryantg <gratzer@fastmail.com>
2022-05-16 11:24:37 -0700

    edited apipie description

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -10,5 +10,6 @@ Apipie.configure do |config|
     If you use this API for something cool, please let us know, we like looking at interesting uses of the data.
     If you have any suggestions/requests for endpoints, please email: admin@pinballmap.com.
     If you have any patches that you'd like to submit to the API, please check out: https://github.com/pinballmap/pbm.
+    If you use this data, please include attribution.
   "
 end
