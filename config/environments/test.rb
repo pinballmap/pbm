ea50a1da4d79b89f10dca0e1ae1294f85d337967
ryantg <gratzer@fastmail.com>
2022-05-24 10:58:54 -0700

    raise deprecation warnings in log

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  # config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :raise
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
