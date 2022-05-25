33e8927b1fcf82b9a8b9e55b0149b251fcf8e53e
ryantg <gratzer@fastmail.com>
2022-05-24 21:04:21 -0700

    bundle update rails_admin 1.4.3 -> 2.21

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Pbm
   class Application < Rails::Application
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
