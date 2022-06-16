e8db65f906930db9f4ffeb895cbf50b2ae248231
RyanTG <gratzer@fastmail.com>
2022-06-15 20:15:06 -0700

    new geocoder config syntax, and try nominatim on staging

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,18 +1,16 @@
-# geocoding service:
-Geocoder::Configuration.lookup = :google
+Geocoder.configure(
+  # street address geocoding service (default :nominatim)
+  # lookup: :google,
+  lookup: :nominatim,
 
-# to use an API key:
-Geocoder::Configuration.api_key = ENV['GOOGLE_MAPS_API_KEY'] ? ENV['GOOGLE_MAPS_API_KEY'] : ''
+  # to use an API key:
+  # api_key: ENV['GOOGLE_MAPS_API_KEY'] ? ENV['GOOGLE_MAPS_API_KEY'] : '',
 
-# geocoding service request timeout, in seconds (default 3):
-Geocoder::Configuration.timeout = 20
+  # geocoding service request timeout, in seconds (default 3):
+  timeout: 20,
 
-# use HTTPS for geocoding service connections:
-Geocoder::Configuration.use_https = false
+  use_https: false,
 
-# language to use (for search queries and reverse geocoding):
-Geocoder::Configuration.language = :en
+  language: :en
 
-# caching (see below for details)
-#Geocoder::Configuration.cache = Redis.new
-#Geocoder::Configuration.cache_prefix = "..."
+)
