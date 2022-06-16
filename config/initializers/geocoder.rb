6c5dd62d79c62259c12e18b7ded2d25dd9f19c87
RyanTG <gratzer@fastmail.com>
2022-06-15 20:35:53 -0700

    back to google maps geocoder
    To test usage with my api key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,10 +1,10 @@
 Geocoder.configure(
   # street address geocoding service (default :nominatim)
-  # lookup: :google,
-  lookup: :nominatim,
+  lookup: :google,
+  # lookup: :nominatim,
 
   # to use an API key:
-  # api_key: ENV['GOOGLE_MAPS_API_KEY'] ? ENV['GOOGLE_MAPS_API_KEY'] : '',
+  api_key: ENV['GOOGLE_MAPS_API_KEY'] ? ENV['GOOGLE_MAPS_API_KEY'] : '',
 
   # geocoding service request timeout, in seconds (default 3):
   timeout: 20,
