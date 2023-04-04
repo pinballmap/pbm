9b5801e645933703d76856e2aa79eec06fa47940
RyanTG <gratzer@fastmail.com>
2023-04-03 21:14:27 -0700

    n+1 on events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,7 @@ class EventsController < InheritedResources::Base
   end
 
   def index
-    @events = apply_scopes(Event.includes(:location))
+    @events = apply_scopes(Event)
     @events.select!(&:active?)
 
     respond_to do |format|
