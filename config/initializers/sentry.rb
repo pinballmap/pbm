9583ab07f122dbdf1070824595adcfe13d35546b
RyanTG <gratzer@fastmail.com>
2023-04-18 19:40:24 -0700

    sentry tweaks

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,10 +5,10 @@ Sentry.init do |config|
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    config.traces_sample_rate = 1.0
+    config.traces_sample_rate = 0.5
     # or
     # config.traces_sampler = lambda do |context|
     #     true
     # end
-    # config.enabled_environments = ['production']
+    config.enabled_environments = ['production']
 end
