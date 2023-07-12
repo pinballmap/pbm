045229d078c157394ace5b914a7f26c36f2f60bf
Ben Harris <ben@tilde.team>
2023-07-12 13:09:42 -0400

    fix status and banned_ip migrations

--- db/migrate/20230504162149_create_status.rb
+++ db/migrate/20230504162149_create_status.rb
@@ -1,6 +1,6 @@
 class CreateStatus < ActiveRecord::Migration[7.0]
   def change
-    create_table :statuses do |t|
+    create_table :statuses, if_not_exists: true do |t|
       t.string :status_type
       t.timestamps
     end
--- db/migrate/20230702162416_create_banned_ips_again.rb
+++ db/migrate/20230702162416_create_banned_ips_again.rb
@@ -1,6 +1,6 @@
 class CreateBannedIpsAgain < ActiveRecord::Migration[7.0]
   def change
-    create_table :banned_ips do |t|
+    create_table :banned_ips, if_not_exists: true do |t|
       t.string :ip_address
 
       t.timestamps
