ed931efec2069ac4718f33b6960a231e8e9c8756
RyanTG <gratzer@fastmail.com>
2023-07-13 17:03:47 -0700

    Have redo respect location type and num machines filters

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -6,7 +6,7 @@ module Api
 
       before_action :allow_cors
       respond_to :json
-      has_scope :by_location_name, :by_location_id, :by_machine_id, :by_machine_name, :by_city_id, :by_state_id, :by_zone_id, :by_operator_id, :by_type_id, :by_machine_single_id, :by_machine_group_id, :by_at_least_n_machines_city, :by_at_least_n_machines_zone, :by_at_least_n_machines_type, :region, :by_ipdb_id, :by_opdb_id, :by_is_stern_army, :regionless_only, :manufacturer
+      has_scope :by_location_name, :by_location_id, :by_machine_id, :by_machine_name, :by_city_id, :by_state_id, :by_zone_id, :by_operator_id, :by_type_id, :by_machine_single_id, :by_machine_group_id, :by_at_least_n_machines, :by_at_least_n_machines_city, :by_at_least_n_machines_zone, :by_at_least_n_machines_type, :region, :by_ipdb_id, :by_opdb_id, :by_is_stern_army, :regionless_only, :manufacturer
 
       MAX_MILES_TO_SEARCH_FOR_CLOSEST_LOCATION = 50
 
@@ -64,6 +64,7 @@ module Api
       param :by_zone_id, Integer, desc: 'Zone ID to search by', required: false
       param :by_operator_id, Integer, desc: 'Operator ID to search by', required: false
       param :by_type_id, Integer, desc: 'Location type ID to search by', required: false
+      param :by_at_least_n_machines, Integer, desc: 'Only locations with N or more machines', required: false
       param :by_at_least_n_machines_type, Integer, desc: 'Only locations with N or more machines', required: false
       param :by_is_stern_army, Integer, desc: 'Send only locations labeled as Stern Army', required: false
       param :no_details, Integer, desc: 'Omit lmx/condition data from pull', required: false
@@ -71,7 +72,7 @@ module Api
       param :regionless_only, Integer, desc: 'Show only regionless locations', required: false
       formats ['json']
       def index
-        return return_response(FILTERING_REQUIRED_MSG, 'errors') unless params[:region] || params[:by_location_name] || params[:by_location_id] || params[:by_machine_id] || params[:by_ipdb_id] || params[:by_opdb_id] || params[:by_machine_name] || params[:by_city_id] || params[:by_machine_group_id] || params[:by_zone_id] || params[:by_operator_id] || params[:by_type_id] || params[:by_at_least_n_machines_type] || params[:by_is_stern_army] || params[:regionless_only]
+        return return_response(FILTERING_REQUIRED_MSG, 'errors') unless params[:region] || params[:by_location_name] || params[:by_location_id] || params[:by_machine_id] || params[:by_ipdb_id] || params[:by_opdb_id] || params[:by_machine_name] || params[:by_city_id] || params[:by_machine_group_id] || params[:by_zone_id] || params[:by_operator_id] || params[:by_type_id] || params[:by_at_least_n_machines_type] || params[:by_at_least_n_machines] || params[:by_is_stern_army] || params[:regionless_only]
 
         except = params[:no_details] ? %i[phone website description created_at updated_at date_last_updated last_updated_by_user_id region_id] : nil
 
@@ -149,6 +150,7 @@ module Api
       param :by_type_id, Integer, desc: 'Location type ID to search by', required: false
       param :by_machine_id, Integer, desc: 'Machine ID to find in locations', required: false
       param :by_operator_id, Integer, desc: 'Operator ID to search by', required: false
+      param :by_at_least_n_machines, Integer, desc: 'Only locations with N or more machines', required: false
       param :by_at_least_n_machines_type, Integer, desc: 'Only locations with N or more machines', required: false
       param :max_distance, String, desc: 'Closest location within "max_distance" miles, with a max of 500', required: false
       param :no_details, Integer, desc: 'Omit data that app does not need from pull', required: false
@@ -190,6 +192,7 @@ module Api
       param :by_machine_group_id, String, desc: 'Machine Group to search for', required: false
       param :by_operator_id, Integer, desc: 'Operator ID to search by', required: false
       param :user_faved, Integer, desc: 'User ID of Faved Locations', required: false
+      param :by_at_least_n_machines, Integer, desc: 'Only locations with N or more machines', required: false
       param :by_at_least_n_machines_type, Integer, desc: 'Only locations with N or more machines', required: false
       param :no_details, Integer, desc: 'Omit data that app does not need from pull', required: false
       formats ['json']
