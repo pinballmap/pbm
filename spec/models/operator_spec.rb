a0f57bd6b2db9f8fadb36384e133a9fd071edaf3
RyanTG <gratzer@fastmail.com>
2023-08-07 12:36:30 -0700

    remove region name from operator digest emails

--- spec/models/operator_spec.rb
+++ spec/models/operator_spec.rb
@@ -57,7 +57,42 @@ describe Operator do
       FactoryBot.create(:machine_condition, location_machine_xref: lmx2, comment: 'Old Cleo Comment', created_at: Date.today - 2.days)
 
       body = <<HERE
-Here's a list of comments made on your pinball machines that were posted today to #{@o.region.full_name}. We're sending this in the hope that it will help you identify, and fix, problems. If you don't want to receive these messages, please contact map@pinballmap.com.
+Here's a list of comments made on your pinball machines yesterday on Pinball Map. We're sending this in the hope that it will help you identify, and fix, problems. If you don't want to receive these messages, please contact map@pinballmap.com.
+
+Comment: Sassy Comment
+Location: Cleo Corner - 303 Southeast 3rd Avenue, Portland, OR, 97214
+Machine: Sassy
+
+Comment: Cleo Comment
+Location: Cleo Corner - 303 Southeast 3rd Avenue, Portland, OR, 97214
+Machine: Cleo
+HERE
+      expect(Pony).to receive(:mail) do |mail|
+        expect(mail).to include(
+          body: body,
+          subject: "Pinball Map - Daily Digest of comments made on your machines - #{Date.today.strftime('%m/%d/%Y')}",
+          to: 'foo@bar.com',
+          from: 'admin@pinballmap.com'
+        )
+      end
+
+      @o.send_recent_comments
+    end
+
+    it 'Sends emails to regionless operators with recent comments on their machines' do
+      l = FactoryBot.create(:location, region: nil, operator: @o, name: 'Cleo Corner')
+
+      m1 = FactoryBot.create(:machine, name: 'Sassy')
+      m2 = FactoryBot.create(:machine, name: 'Cleo')
+      lmx1 = FactoryBot.create(:location_machine_xref, location: l, machine: m1)
+      lmx2 = FactoryBot.create(:location_machine_xref, location: l, machine: m2)
+
+      FactoryBot.create(:machine_condition, location_machine_xref: lmx1, comment: 'Sassy Comment', created_at: (Time.now - 1.day).beginning_of_day)
+      FactoryBot.create(:machine_condition, location_machine_xref: lmx2, comment: 'Cleo Comment', created_at: (Time.now - 1.day).beginning_of_day)
+      FactoryBot.create(:machine_condition, location_machine_xref: lmx2, comment: 'Old Cleo Comment', created_at: Date.today - 2.days)
+
+      body = <<HERE
+Here's a list of comments made on your pinball machines yesterday on Pinball Map. We're sending this in the hope that it will help you identify, and fix, problems. If you don't want to receive these messages, please contact map@pinballmap.com.
 
 Comment: Sassy Comment
 Location: Cleo Corner - 303 Southeast 3rd Avenue, Portland, OR, 97214
