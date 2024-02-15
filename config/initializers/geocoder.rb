810fd182ad228694fc606b9ad3b83a233571c03c
RyanTG <gratzer@fastmail.com>
2024-02-14 18:24:23 -0800

    revert

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,12 +1,10 @@
 Geocoder.configure(
   # street address geocoding service (default :nominatim)
-  lookup: :mapbox,
+  lookup: :google,
   # lookup: :nominatim,
 
   # to use an API key:
-  api_key: 'pk.eyJ1IjoicnlhbnRnIiwiYSI6ImNsZTV5dW11YTBoankzbnFsZTEzcnQycWsifQ._0FYgeHFSQCQJ6VboGXl8A',
-  # api_key: ENV['MAPBOX_API_KEY'] ? ENV['MAPBOX_API_KEY'] : '',
-  mapbox: {dataset: "mapbox.places"},
+  api_key: ENV['GOOGLE_MAPS_API_KEY'] ? ENV['GOOGLE_MAPS_API_KEY'] : '',
 
   # geocoding service request timeout, in seconds (default 3):
   timeout: 20,
