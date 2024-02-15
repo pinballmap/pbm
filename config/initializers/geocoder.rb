ddf99959267167a93110dc6e9ee13d55e7c8bf25
RyanTG <gratzer@fastmail.com>
2024-02-14 18:21:00 -0800

    One more mapbox geocoder try with an explicit secured key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,10 +1,12 @@
 Geocoder.configure(
   # street address geocoding service (default :nominatim)
-  lookup: :google,
+  lookup: :mapbox,
   # lookup: :nominatim,
 
   # to use an API key:
-  api_key: ENV['GOOGLE_MAPS_API_KEY'] ? ENV['GOOGLE_MAPS_API_KEY'] : '',
+  api_key: 'pk.eyJ1IjoicnlhbnRnIiwiYSI6ImNsZTV5dW11YTBoankzbnFsZTEzcnQycWsifQ._0FYgeHFSQCQJ6VboGXl8A',
+  # api_key: ENV['MAPBOX_API_KEY'] ? ENV['MAPBOX_API_KEY'] : '',
+  mapbox: {dataset: "mapbox.places"},
 
   # geocoding service request timeout, in seconds (default 3):
   timeout: 20,
