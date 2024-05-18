7c7cf290a89212d7c9429b08f53c05383523d93a
RyanTG <gratzer@fastmail.com>
2024-05-17 19:28:21 -0700

    drop nominatim

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,14 +1,15 @@
 Geocoder.configure(
   # street address geocoding service (default :nominatim)
-  lookup: :google,
-  # lookup: :nominatim,
+  # lookup: :google,
+  lookup: :mapbox,
 
-  # to use an API key:
-  api_key: ENV['GOOGLE_MAPS_API_KEY'] ? ENV['GOOGLE_MAPS_API_KEY'] : '',
+  api_key: ENV['MAPBOX_GEOCODER_API_KEY'] ? ENV['MAPBOX_GEOCODER_API_KEY'] : '',
+
+  # api_key: ENV['GOOGLE_MAPS_API_KEY'] ? ENV['GOOGLE_MAPS_API_KEY'] : '',
 
   # geocoding service request timeout, in seconds (default 3):
   timeout: 20,
-  use_https: false,
+  use_https: true,
   language: :en,
   logger: Rails.logger,
   kernel_logger_level: ::Logger::DEBUG
