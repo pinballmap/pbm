acd84811a6209ecf6195fa1d44ae212c28361d1b
fiveNinePlusR <fiveNinePlusR@gmail.com>
2024-07-08 19:11:10 -0700

    Make dotenv work correctly

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => "localhost:#{ENV.fetch('PORT', 3000)}" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => 'localhost', :port => 1025}
 
