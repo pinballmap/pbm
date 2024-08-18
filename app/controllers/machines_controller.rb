3b70adae63fe5e34706b3572cca3abc36f2d670f
fiveNinePlusR <fiveNinePlusR@gmail.com>
2024-08-18 14:10:21 -0700

    Make the region machine search faster
    When searching for machines use the speed of the database to select
    exactly what we want for autocomplete results.
    
    cleaned up the other if branch for readability

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -13,10 +13,30 @@ class MachinesController < InheritedResources::Base
 
   def autocomplete
     if params[:region_level_search].nil?
-      render json: Machine.where("clean_items(name) ilike '%' || clean_items(?) || '%'", params[:term]).sort_by(&:name).map { |m| { label: m.name_and_year, value: m.name_and_year, id: m.id, group_id: m.machine_group_id } }
+      results = Machine.where("clean_items(name) ilike '%' || clean_items(?) || '%'", params[:term])
+                       .sort_by(&:name)
+                       .map { |m| { label: m.name_and_year, value: m.name_and_year, id: m.id, group_id: m.machine_group_id } }
     else
-      render json: @region.machines.select { |m| m.name_and_year =~ /#{Regexp.escape params[:term] || ''}/i }.sort_by(&:name).map { |m| { label: m.name_and_year, value: m.name_and_year, id: m.id, group_id: m.machine_group_id } }
+      sql = <<-SQL
+      select distinct m.name, m."year", m.id, m.machine_group_id, m.manufacturer from locations l
+      inner join location_machine_xrefs lmx on lmx.location_id = l.id
+      inner join machines m on m.id = lmx.machine_id
+      where region_id = 1 and (clean_items(m.name)) ilike '%' || clean_items(:term) || '%'
+      order by m.name
+      SQL
+
+      sanitized_sql = ActiveRecord::Base.sanitize_sql_array([sql, { term: params[:term] }])
+
+      results = ActiveRecord::Base.connection.select_all(sanitized_sql)
+        .map do |m|
+          name_year = "#{m['name']} (#{m['manufacturer']}, #{m['year']})"
+
+          { label: name_year, value: name_year, id: m['id'], group_id: m['machine_group_id'] }
+        end
+
     end
+
+    render json: results
   end
 
   def index
