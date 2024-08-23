14335e38f4c1314961d4d6288a0006b78c39be2e
RyanTG <gratzer@fastmail.com>
2024-08-22 17:38:06 -0700

    Another attempt at silencing backtrace

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,5 +5,5 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code
 # by setting BACKTRACE=1 before calling your invocation, like "BACKTRACE=1 ./bin/rails runner 'MyClass.perform'".
-Rails.backtrace_cleaner.remove_silencers! if ENV["BACKTRACE"]
-Rails.backtrace_cleaner.add_silencer {|line| line =~ /gems/}
+# Rails.backtrace_cleaner.remove_silencers! if ENV["BACKTRACE"]
+Rails.backtrace_cleaner.add_silencer {||line| /vendor/.match?(line)}
