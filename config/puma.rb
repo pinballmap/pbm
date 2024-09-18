f350fe33e33006059dc6de66be38a851834352e1
RyanTG <gratzer@fastmail.com>
2024-09-18 09:37:14 -0700

    Adding back worker number to puma even though it's also set by env variable

--- config/puma.rb
+++ config/puma.rb
@@ -23,6 +23,8 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
