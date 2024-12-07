207ed0156aa9cae857177c41d7e20bba216e31a5
fiveNinePlusR <fiveNinePlusR@gmail.com>
2024-12-06 20:23:32 -0800

    Fix migrations and use env vars for db name if set

--- db/migrate/20241126054618_add_submission_counts_to_users.rb
+++ db/migrate/20241126054618_add_submission_counts_to_users.rb
@@ -1,9 +1,9 @@
 class AddSubmissionCountsToUsers < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :num_machines_added, :integer
-    add_column :users, :num_machines_removed, :integer
-    add_column :users, :num_locations_suggested, :integer
-    add_column :users, :num_lmx_comments_left, :integer
-    add_column :users, :num_msx_scores_added, :integer
+    add_column :users, :num_machines_added, :integer, default: 0
+    add_column :users, :num_machines_removed, :integer, default: 0
+    add_column :users, :num_locations_suggested, :integer, default: 0
+    add_column :users, :num_lmx_comments_left, :integer, default: 0
+    add_column :users, :num_msx_scores_added, :integer, default: 0
   end
 end
