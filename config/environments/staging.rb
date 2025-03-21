4e0db9f7ce527a0f1ce4edaeaf457a63a83ded33
RyanTG <gratzer@fastmail.com>
2025-03-21 10:49:15 -0700

    comment config.solid_queue.connects_to

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :async
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  # config.solid_queue.connects_to = { database: { writing: :queue } }
   # config.active_job.queue_name_prefix = "pbm_production"
 
   # Suppress logger output for asset requests.
