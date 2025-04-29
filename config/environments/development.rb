375af61e91667bdf0289a110127b8b3c9f6044c9
RyanTG <gratzer@fastmail.com>
2025-04-29 15:54:36 -0700

    Restore aws uploads in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
   config.cache_store = :memory_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-  # config.active_storage.variant_processor = :vips
+  config.active_storage.service = :amazon
+  config.active_storage.variant_processor = :vips
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
