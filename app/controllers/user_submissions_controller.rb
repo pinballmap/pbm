42f1c51da2adbd80e801d29244df0fae68ae0f85
Alex Ose <alex.ose@a8c.com>
2025-11-14 11:49:18 -0800

    Initial attempt

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -2,9 +2,11 @@ class UserSubmissionsController < ApplicationController
   has_scope :region
 
   def list_within_range
+    bounds = [ params[:boundsData][:sw][:lat], params[:boundsData][:sw][:lng],
+               params[:boundsData][:ne][:lat], params[:boundsData][:ne][:lng] ]
     user_submissions = UserSubmission.where.not(lat: nil)
                                      .where(submission_type: %w[new_lmx remove_machine new_condition confirm_location], created_at: "2019-05-03T07:00:00.00-07:00"..Date.today.end_of_day)
-                                     .near([ params[:lat], params[:lon] ], 100, order: false).limit(200)
+                                     .within_bounding_box(bounds).limit(200)
     sorted_submissions = user_submissions.order("created_at DESC")
     render partial: "maps/nearby_activity", locals: { sorted_submissions: sorted_submissions }
   end
