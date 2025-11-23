46504cecfd6f449b20409fa3d488d6d46086fc1c
alexose <alexose@users.noreply.github.com>
2025-11-23 08:35:15 -0800

    Add pagination to nearby activity
    Add an overall limit of 2000 results
    
    Add plus
    
    Remove formatting from ellipsis
    
    Blah
    
    Move javascript over to map.html.haml

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -6,8 +6,10 @@ class UserSubmissionsController < ApplicationController
                params[:boundsData][:ne][:lat], params[:boundsData][:ne][:lng] ]
     user_submissions = UserSubmission.where.not(lat: nil)
                                      .where(submission_type: %w[new_lmx remove_machine new_condition confirm_location], created_at: "2019-05-03T07:00:00.00-07:00"..Date.today.end_of_day, deleted_at: nil)
-                                     .within_bounding_box(bounds).limit(200)
-    sorted_submissions = user_submissions.order("created_at DESC")
-    render partial: "maps/nearby_activity", locals: { sorted_submissions: sorted_submissions }
+                                     .within_bounding_box(bounds)
+                                     .order("created_at DESC")
+                                     .limit(2000)
+    @pagy, sorted_submissions = pagy(user_submissions, items: 10, limit_extra: false)
+    render partial: "maps/nearby_activity", locals: { sorted_submissions: sorted_submissions, pagy: @pagy }
   end
 end
