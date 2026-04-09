d30ac61d7fd3b65fb8fc1a4ccaaf327faef871a7
RyanTG <gratzer@fastmail.com>
2026-04-09 10:58:50 -0700

    eager_load=true in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.enable_reloading = true
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
