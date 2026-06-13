bb5e89da978b607577e6bc182bc1f3d7d762c078
RyanTG <gratzer@fastmail.com>
2026-06-13 16:05:23 -0700

    update api docs description

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -7,9 +7,11 @@ Apipie.configure do |config|
   config.translate               = false
   config.api_routes              = Rails.application.routes
   config.app_info = "
-    If you use this API for something cool, please let us know, we like looking at interesting uses of the data.
-    If you have any suggestions/requests for endpoints, please email: admin@pinballmap.com.
-    If you have any patches that you'd like to submit to the API, please check out: https://github.com/pinballmap/pbm.
-    If you use this data, please include attribution.
+    If you use this API for something cool, please let us know, we like looking at interesting uses of the data.<br><br>
+    If you have any suggestions/requests for endpoints, please email: admin@pinballmap.com.<br><br>
+    If you have any patches that you'd like to submit to the API, please check out: https://github.com/pinballmap/pbm.<br><br>
+    If you use this data, please include attribution.<br><br>
+    If you are making thousands of requests in a short amount of time, you are doing something wrong (there's always a better way) and you might get blocked.<br><br>
+    Check https://pinballmap.com/llms.txt for more tips.
   "
 end
