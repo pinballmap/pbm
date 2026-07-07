daa1c54084a61048dffcb51ad3f94596a3ee1aed
RyanTG <gratzer@fastmail.com>
2026-07-07 07:36:00 -0700

    No auth needed for random machine function

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,7 @@ class LocationsController < ApplicationController
   respond_to :html, only: %i[index]
   has_scope :by_location_name, :by_city_id, :by_at_least_n_machines, :by_at_least_n_machines_city, :by_at_least_n_machines_zone, :by_at_least_n_machines_type, :by_city_name, :by_city_no_state, :by_center_point_and_ne_boundary, :by_is_stern_army, :by_ic_active, :user_faved, :by_machine_name, :region, :by_machine_year_gte, :by_machine_year_lte
   has_scope :by_type_id, :by_location_id, :by_operator_id, :by_zone_id, :by_machine_id, :by_machine_single_id, :by_machine_group_id, :by_machine_id_ic, :by_machine_single_id_ic, :by_machine_year, :by_ipdb_id, :by_opdb_id, :manufacturer, :by_machine_type, :by_machine_display, :by_country, :by_state_name, :by_state_id, type: :array
-  before_action :authenticate_user!, except: %i[index autocomplete autocomplete_city render_machines render_machines_count render_last_updated render_location_detail render_recent_activity sanitize_integers]
+  before_action :authenticate_user!, except: %i[index autocomplete autocomplete_city render_machines render_machines_count render_last_updated render_location_detail render_recent_activity sanitize_integers random_machine]
   before_action :normalize_array_params
   rate_limit to: 100, within: 1.minute, only: :index, name: "locations_index"
   rate_limit to: 12, within: 3.seconds, only: :render_location_detail, name: "locations_render_location_detail"
