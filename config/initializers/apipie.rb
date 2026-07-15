5ce5a3dc47ca2c8997c69fd72a7a41a3ba918292
RyanTG <gratzer@fastmail.com>
2026-07-15 16:35:04 -0700

    Clarified data use agreement

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -7,11 +7,12 @@ Apipie.configure do |config|
   config.translate               = false
   config.api_routes              = Rails.application.routes
   config.app_info = "
-    If you use this API for something cool, please let us know, we like looking at interesting uses of the data.<br><br>
-    If you have any suggestions/requests for endpoints, please email: admin@pinballmap.com.<br><br>
-    If you have any patches that you'd like to submit to the API, please check out: https://github.com/pinballmap/pbm.<br><br>
-    If you use this data, please include attribution.<br><br>
+    The Pinball Map API is for services that want to extend beyond our offered functionality, or do something neat with the data. It is not for creating a clone where people can find machines to play. If you create a clone, that is against our data usage agreement and you will likely be blocked.<br><br>
+    If you use this data, include attribution.<br><br>
     If you are making thousands of requests in a short amount of time, you are doing something wrong (there's always a better way) and you might get blocked.<br><br>
-    Check https://pinballmap.com/llms.txt for more tips.
+    Check https://pinballmap.com/llms.txt for more tips.<br><br>
+    If you use this API for something cool, let us know. We like looking at interesting uses of the data.<br><br>
+    If you have any suggestions/requests for endpoints, please email: admin@pinballmap.com.<br><br>
+    If you have any patches that you'd like to submit to the API, please check out: https://github.com/pinballmap/pbm.
   "
 end
